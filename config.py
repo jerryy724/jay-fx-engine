ec8ccd0ae5dd3b1f55e261b89ea583db39d3d445
jerryy724 <jerryjohncolley@gmail.com>
2026-08-15 10:20:24 +0000

    Update config.py

--- config.py
+++ config.py
@@ -5,15 +5,15 @@ CHAT_ID = os.getenv("CHAT_ID")
 TWELVE_DATA_API_KEY = os.getenv("TWELVE_DATA_API_KEY")
 
 FX_ROTATION = [
-    {"name": "EUR/USD", "symbol": "EUR/USD", "type": "FX"},
-    {"name": "GBP/USD", "symbol": "GBP/USD", "type": "FX"},
-    {"name": "USD/JPY", "symbol": "USD/JPY", "type": "FX"},
-    {"name": "AUD/USD", "symbol": "AUD/USD", "type": "FX"},
-    {"name": "USD/CAD", "symbol": "USD/CAD", "type": "FX"},
-    {"name": "USD/CHF", "symbol": "USD/CHF", "type": "FX"}
+    {"name": "EUR/USD", "symbol": "EUR/USD", "type": "FX", "default_direction": "BUY"},
+    {"name": "GBP/USD", "symbol": "GBP/USD", "type": "FX", "default_direction": "SELL"},
+    {"name": "USD/JPY", "symbol": "USD/JPY", "type": "FX", "default_direction": "BUY"},
+    {"name": "AUD/USD", "symbol": "AUD/USD", "type": "FX", "default_direction": "SELL"},
+    {"name": "USD/CAD", "symbol": "USD/CAD", "type": "FX", "default_direction": "BUY"},
+    {"name": "USD/CHF", "symbol": "USD/CHF", "type": "FX", "default_direction": "SELL"}
 ]
 
 CRYPTO_ROTATION = [
-    {"name": "BTC/USD", "symbol": "BTC/USD", "type": "CRYPTO"},
-    {"name": "ETH/USD", "symbol": "ETH/USD", "type": "CRYPTO"}
+    {"name": "BTC/USD", "symbol": "BTC/USD", "type": "CRYPTO", "default_direction": "SELL"},
+    {"name": "ETH/USD", "symbol": "ETH/USD", "type": "CRYPTO", "default_direction": "BUY"}
 ]
